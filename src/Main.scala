e9f4f4b72d84051a2ccdf4f806139d53f282f178
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-03-28 20:30:04 +0000

    Update Main.scala

--- src/Main.scala
+++ src/Main.scala
@@ -1,7 +1,19 @@
+import Utils.{Board, fillOneCell, randomChar}
 
 object Main {
   def main(args: Array[String]): Unit = {
+    // Example of usage:
+    val board: Board = List(
+      List('A', 'B', 'C'),
+      List('D', 'E', 'F'),
+      List('G', 'H', 'I')
+    )
+    val newBoard = fillOneCell(board, 'X', (2, 2)) // Changing the element at position (1,1) to 'X'
 
-
+    newBoard.foreach(row => println(row.mkString(" ")))
   }
+
+
+
+
 }
