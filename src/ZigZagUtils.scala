ef7e7ba3c1ccc40ded378cf7b082bb36e7a0de52
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-04-10 22:03:03 +0100

    Update ZigZagUtils.scala

--- src/ZigZagUtils.scala
+++ src/ZigZagUtils.scala
@@ -8,10 +8,12 @@ object ZigZagUtils {
   type Board = List[List[Char]]
   private type Coord2D = (Int, Int) //(linha, coluna)
 
+  // Direções possíveis
   object Direction extends Enumeration {
     type Direction = Value
-    private val North, South, East, West, NorthEast, NorthWest, SouthEast, SouthWest = Value
+    val North, South, East, West, NorthEast, NorthWest, SouthEast, SouthWest = Value
 
+    // Retorna a coordenada seguinte dada uma coordenada e direção
     def nextCoord(coord: Coord2D, direction: Direction.Value): Coord2D = direction match {
       case Direction.North => (coord._1 - 1, coord._2)
       case Direction.NorthEast => (coord._1 - 1, coord._2 + 1)
@@ -23,29 +25,30 @@ object ZigZagUtils {
       case Direction.NorthWest => (coord._1 - 1, coord._2 - 1)
     }
 
-
+    // Converte uma string em uma direção
+    // Usar Option da nos um modo de lidar com a ausência de elementos
     def stringToDirection(input: String): Option[Direction.Value] = {
       Direction.values.find(_.toString.equalsIgnoreCase(input))
     }
   }
 
 
-
-
-
-
+  // Gera um caractere aleatório
   def randomChar(rand: MyRandom): (Char, MyRandom) = {
     rand.nextChar
   }
 
+  // Preenche uma posição do tabuleiro com um caractere específico
   private def fillOneCell(board: Board, letter: Char, coord: Coord2D): Board = {
 
+    // Preenche uma linha do tabuleiro
     def fillRow(row: List[Char], columnIndex: Int): List[Char] = row match {
       case Nil => Nil
       case _ :: tail if columnIndex == 0 => letter :: tail
       case head :: tail => head :: fillRow(tail, columnIndex - 1)
     }
 
+    // Preenche o tabuleiro
     def fillBoard(board: Board, rowIndex: Int): Board = board match {
       case Nil => Nil
       case head :: tail if rowIndex == 0 => fillRow(head, coord._2) :: tail
@@ -56,14 +59,17 @@ object ZigZagUtils {
   }
 
 
+  // Preenche o tabuleiro com as palavras nas posições dadas
   private def setBoardWithWords(board: Board, words: List[String], positions: List[List[Coord2D]]): Board = {
 
+    // Preenche o tabuleiro nas posições dadas com a letra correspondente
     @tailrec
     def fillWord(board: Board, word: String, positions: List[Coord2D], index: Int): Board = positions match {
       case Nil => board
       case head :: tail => fillWord(fillOneCell(board, word(index), head), word, tail, index + 1)
     }
 
+    // Preenche o tabuleiro com as palavras dadas
     @tailrec
     def fillAllWords(board: Board, words: List[String], positions: List[List[Coord2D]], index: Int): Board = words match {
       case Nil => board
@@ -73,9 +79,10 @@ object ZigZagUtils {
     fillAllWords(board, words, positions, 0)
   }
 
-
+// Preenche o tabuleiro com caracteres aleatórios
   def completeBoardRandomly(board: Board, r: MyRandom, f: MyRandom => (Char, MyRandom)): (Board, MyRandom) = {
 
+    // Preenche uma linha do tabuleiro com caracteres aleatorios
     def completeBoardFillRow(row: List[Char], r: MyRandom): (List[Char], MyRandom) = {
       row.foldLeft((List[Char](), r)) {
         //case (accumulator, currentElement)
@@ -85,25 +92,30 @@ object ZigZagUtils {
       }
     }
 
-    def completeBoardAux(board: Board, r: MyRandom): (Board, MyRandom) = board match {
+    //Prenche o tabuleiro
+    def completeBoard(board: Board, r: MyRandom): (Board, MyRandom) = board match {
       case Nil => (Nil, MyRandom(System.currentTimeMillis()))
       case head :: tail =>
         val (newHead, r1) = completeBoardFillRow(head, r)
-        val (newTail, r2) = completeBoardAux(tail, r1)
+        val (newTail, r2) = completeBoard(tail, r1)
         (newHead :: newTail, r2)
     }
 
-    completeBoardAux(board, r)
+    completeBoard(board, r)
   }
 
-
+// Joga a palavra, na posição inicial, segundo a direção dada
   def play(board: Board, word: String, start: Coord2D, direction: Direction.Value): Boolean = {
     val file: String = "src/givenWords.txt"
+
+    // Extrai todas as palavras e respetivas posições do ficheiro
     val (words, positions) = readFromFile(file)
+
+    // Procura a palavra jogada nas palavras extraidas do ficheiro
+    // Quando encontrar chama a função "checkWordIsInBoard"
     @tailrec
     def searchForTheGivenWordInTheList(wordsList: List[String], coordsList: List[List[Coord2D]], coordAnswer: Coord2D, wordAnswer: String, direction: Direction.Value): Boolean = wordsList match {
-      case Nil =>
-        false
+      case Nil => false
       case x :: xs =>
         if (x == wordAnswer && coordsList.head.head == coordAnswer && Direction.nextCoord(coordAnswer, direction) == coordsList.head.tail.head) {
           checkWordIsInBoard(board, wordAnswer, coordsList.head)
@@ -112,6 +124,7 @@ object ZigZagUtils {
         }
     }
 
+    // Verifica se para cada posição da palavra está a letra suposta
     @tailrec
     def checkWordIsInBoard(board: Board, wordAnswer: String, coordsList: List[Coord2D]): Boolean = coordsList match {
       case Nil => true
@@ -127,54 +140,59 @@ object ZigZagUtils {
   }
 
 
+  // Extrai as palavras e respetivas coordenadas do ficheiro
   private def readFromFile(file: String): (List[String], List[List[Coord2D]]) = {
     val bufferedSource = Source.fromFile(file)
+
+    //Extrai o conteudo
     val content: List[String] = bufferedSource.getLines.mkString("\n").split("\n").toList
     bufferedSource.close()
 
+    // Padrões regex
     val coordPattern: Regex = "\\(([0-9]+),([0-9]+)\\)".r
     val wordPattern: Regex = "^[a-zA-Z]+$".r
 
     @tailrec
     def getFormatFromList( content: List[String], words: List[String], coordinates: List[List[Coord2D]], currentCoords: List[Coord2D]): (List[String], List[List[Coord2D]]) = content match {
       case Nil =>
-        // If there are leftover coordinates, add them to the list; otherwise, just return what we have
+        // Se ainda houverem coordenadas, adiciona-as à lista
+        // Caso contrário devolve o que já temos
         if (currentCoords.nonEmpty) (words, coordinates :+ currentCoords) else (words, coordinates)
       case x :: xs => x match {
         case "" =>
-          // Empty string, indicating end of current word's coordinates
+          // String vazia, indica o fim das coordenadas da palavra atual
           getFormatFromList(xs, words, coordinates :+ currentCoords, List.empty)
         case coordPattern(y, x) =>
-          // Coordinate line
+          // Caso encontremos o padrão das coordenadas
+          // Adicionamo-la às coordenadas da palavra atual
           val newCoord = (x.toInt, y.toInt)
           getFormatFromList(xs, words, coordinates, currentCoords :+ newCoord)
         case _ if wordPattern.matches(x) =>
-          var word = x.toUpperCase
-          // Word line
-          if (currentCoords.isEmpty) {
-            // No current coordinates, just add the word
-            getFormatFromList(xs, words :+ word, coordinates, currentCoords)
-          } else {
-            // There are some coordinates, finalize them and start fresh for the new word
-            getFormatFromList(xs, words :+ word, coordinates :+ currentCoords, List.empty)
-          }
+          // Caso encontremos o padrão das palavras
+          // Adicionamo-la à lista das palavras
+          val word = x.toUpperCase
+          getFormatFromList(xs, words :+ word, coordinates, currentCoords)
         case _ =>
-          // Ignore any line that doesn't match expected patterns (could log or handle errors here)
+          //Qualquer linha fora do esperado, é ignorada
           getFormatFromList(xs, words, coordinates, currentCoords)
       }
     }
-    // Start the recursive processing
     getFormatFromList(content, List(), List(), List())
-
   }
 
 
+  // Inicializa o tabuleiro com as palavras do ficheiro
   def initializeGameBoardWithWordsFromFile(board: Board): Board = {
     val file: String = "src/givenWords.txt"
     val (words, positions) = readFromFile(file)
 
     setBoardWithWords(board, words, positions)
   }
+
+
+  // O resto das funções apenas mostram frases do ecrã e são auto-explicativas
+
+
 
   def showPrompt(): Unit = {
     print("\nGuess a word: ")
